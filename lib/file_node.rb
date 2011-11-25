957420b0b81207c515578f1fd641168c508def15
Jonathan Birkholz <jb@chaione.com>
2011-11-25 17:40:49 -0600

    fixed kevins bug

--- lib/file_node.rb
+++ lib/file_node.rb
@@ -5,17 +5,27 @@ module Boxnet
     def process_node(data)
       @files = []
       @folders = []
-      
+            
       if data["folder"]
         self.folders << Folder.new(data["folder"])
       elsif data["folders"]
-        data["folders"]["folder"].each do |folder|
+        folder = data["folders"]["folder"]
+        if folder.class == Array
+          folder.each do |f|
+            self.folders << Folder.new(f)
+          end
+        elsif folder.class == Hash
           self.folders << Folder.new(folder)
         end
       end
       
       if data["files"]
-        data["files"]["file"].each do |file|
+        file = data["files"]["file"]
+        if file.class == Array
+          file.each do |f|
+            self.files << File.new(f)
+          end
+        elsif file.class == Hash
           self.files << File.new(file)
         end
       end
--- spec/get_account_file_tree_spec.rb
+++ spec/get_account_file_tree_spec.rb
@@ -3,17 +3,22 @@ require 'spec_helper'
 describe "get account tree" do
   before(:each) do
     Boxnet::Client.API_KEY = ENV["BOXNET_API_KEY"]
-    @auth_token = ENV["BOXNET_AUTHTOKEN"]
   end
   
  context "entire tree from root" do
-   before(:each) do       
+   before(:each) do
+     @auth_token = "d402qzspvmk46zo6l59kcplaidmza76o"
      @result = Boxnet::Client.get_account_tree(@auth_token, "0")
    end
    it { @result.should_not be_nil }
-   it { @result.status.should == "listing_ok" }
-   it { @result.file_tree.should_not be_nil }
-   it { @result.file_tree.folders.count.should > 0 }
-   it { @result.file_tree.folders.first.folders.first.files.count.should > 0 }
  end
+ 
+ context "entire tree from root" do
+   before(:each) do
+     @auth_token = "vgp6hb8kjpxbctlmrrigdmvz2fyqpvib"
+     @result = Boxnet::Client.get_account_tree(@auth_token, "0")
+   end
+   it { @result.should_not be_nil }
+ end
+ 
 end
