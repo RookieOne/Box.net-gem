0516e3e6e41b11f86afc10e05bbceb0226f9155c
Jonathan Birkholz <jb@chaione.com>
2011-12-05 09:44:03 -0600

    removed that nil check

--- lib/file_node.rb
+++ lib/file_node.rb
@@ -3,20 +3,23 @@ module Boxnet
     attr :files, :folders
     
     def process_node(data)
-      return data.nil?
+      # return data.nil?
       @files = []
       @folders = []      
       
       if data.has_key?("folder")
+        puts "1" if data["folder"].nil?
         self.folders << Folder.new(data["folder"])
       elsif data.has_key?("folders")
         folder = data["folders"]["folder"]
         
         if folder.class == Array
           folder.each do |f|
+                    puts "2" if f.nil?
             self.folders << Folder.new(f)
           end
         elsif folder.class == Hash
+                  puts "3" if folder.nil?
           self.folders << Folder.new(folder)
         end
       end
@@ -26,9 +29,11 @@ module Boxnet
         
         if file.class == Array
           file.each do |f|
+                    puts "4" if f.nil?
             self.files << File.new(f)
           end
         elsif file.class == Hash
+                  puts "5" if file.nil?
           self.files << File.new(file)
         end
       end
